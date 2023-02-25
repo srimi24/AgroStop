98e84c783210bab2b6fb85b50a0e02353678b3c4
gantasrilaitha <113108071+gantasrilaitha@users.noreply.github.com>
2023-02-25 12:39:05 +0530

    Update water.ino

--- aurdino/water.ino
+++ aurdino/water.ino
@@ -1,9 +1,10 @@
+// connect led & water level sensor to aurduino
 #define ledPin 6
 #define sensorPin A0
 void setup() {
-  // put your setup code here, to run once:
+  
 
-  Serial.begin(9600);
+  Serial.begin(9600);//use 9600 baud to print output
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
 
@@ -11,11 +12,11 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
  
   int sensorValue = analogRead(sensorPin);
   if (sensorValue > 100)  {
-    int outputValue = map(sensorValue, 100, 800, 0, 255);
+    int outputValue = map(sensorValue, 100, 800, 0, 255); // use map function to glow led based on amount of water present in soil
     Serial.println(outputValue);
     analogWrite(ledPin, outputValue); // generate PWM signal
   
